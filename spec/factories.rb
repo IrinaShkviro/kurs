b7a3101b3e955cc44f9abcb551491e01d18164e8
IrinaShkviro <IrinaShkviro13@gmail.com>
2014-04-11 21:11:33 +0400

    Finish user edit, update, index, destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    name     "Michael"
-    surname  "Hartl"
-    group    "111"
-    email    "michael@example.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:surname) { "Test" }
+    sequence(:group) { 111 }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+	    admin true
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe User do
   before do
-    @user = User.new(name: "Example User", email: "user@example.com",
+    @user = User.new(name: "Example", surname: "User", group: 0, 
+		     email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
 
@@ -11,6 +12,20 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
